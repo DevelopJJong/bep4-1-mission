1ef994adea5af682c409d859c720d48e192cea63
이현종 <101362306+DevelopJJong@users.noreply.github.com>
2025-12-24 10:14:54 +0900

    0009

--- src/main/java/com/back/boundedContext/post/app/PostFacade.java
+++ src/main/java/com/back/boundedContext/post/app/PostFacade.java
@@ -3,6 +3,7 @@ package com.back.boundedContext.post.app;
 import com.back.boundedContext.member.domain.Member;
 import com.back.boundedContext.post.domain.Post;
 import com.back.boundedContext.post.out.PostRepository;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -14,14 +15,17 @@ public class PostFacade {
     private final PostRepository postRepository;
     private final PostCreateUseCase postCreateUseCase;
 
+    @Transactional
     public long count() {
         return postRepository.count();
     }
 
+    @Transactional
     public Post create(String title, String content, Member member) {
         return  postCreateUseCase.create(title, content, member);
     }
 
+    @Transactional
     public Optional<Post> findByPostId(int id) {
         return postRepository.findById(id);
     }
